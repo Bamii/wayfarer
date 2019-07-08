906cf260327b50007a03a54d6d6e5af22fe5b53c
bamii <bbamii@outlook.com>
2019-07-08 02:19:16 +0100

    init

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 app.set('views', path.join(__dirname, 'src/views'));
-app.set('view-engine', 'pug');
+app.set('view engine', 'pug');
 
 // App routes
 app.get('/', (req, res) => {
-  res.sendfile(path.join(__dirname, '/views/index.html'));
+  res.render('index');
 });
 
 // Listening Port
 app.listen(8888, () => {
-  debug('Listening on port 3000');
+  debug(`Listening on port ${chalk.green('8888')}`);
 });

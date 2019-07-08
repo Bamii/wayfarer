6851694d87b057cdd710102b287f923f0c1ce0ed
bamii <bbamii@outlook.com>
2019-07-08 14:51:37 +0100

    new commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,6 @@ module.exports = {
   parser: 'babel-eslint',
   rules: {
     strict: 0,
-    'react/jsx-one-expression-per-line': 'off',
-    'jsx-a11y/label-has-associated-control': [
-      'error',
-      {
-        required: {
-          some: ['nesting', 'id']
-        }
-      }
-    ],
-    'jsx-a11y/label-has-for': [
-      'error',
-      {
-        required: {
-          some: ['nesting', 'id']
-        }
-      }
-    ],
     'no-unused-vars': 'warn',
     'import/prefer-default-export': 'warn'
   }

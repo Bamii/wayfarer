80a664243a3357d5305c6da535d1d1294c1e0af4
bamii <bbamii@outlook.com>
2019-07-08 14:43:10 +0100

    setting up testing

---
+++ test/server.js
@@ -0,0 +1,10 @@
+const { assert, should, expect } = require('chai');
+const server = require('../server');
+
+describe('My Server', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+function getVersionNumber() {
+  return 1;
+}
+
+module.exports = {
+  getVersionNumber,
+};

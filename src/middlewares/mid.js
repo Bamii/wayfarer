fde916668c2c7d2af5781ec49fad2bf6539ba32c
Bami <bbamii@outlook.com>
2019-11-26 09:38:55 +0100

    mv

--- src/middlewares/mid.js
+++ src/middlewares/mid.js
@@ -3,25 +3,24 @@ const { is } = require('../utils/helpers');
 
 function middleware(options) {
   const { app, rootMiddleWares, path, paths, auth, baseUrl } = options;
+  // root middleware
+  for (let rm of rootMiddleWares) {
+    if (is('array', rm)) {
+      app.use(rm[0], rm[1]);
+    } else {
+      app.use(`${baseUrl}/*`, rm);
+    }
+  }
+
   if (auth) {
     const { middleware: authMiddleware, paths: authPaths } = auth;
-    
-    // root middleware
-    for (let rm of rootMiddleWares) {
-      if (is('array', rm)) {
-        app.use(rm[0], rm[1]);
-      } else {
-        app.use(`${baseUrl}/*`, rm);
-      }
-    }
 
     // auth middleware
     for (let p of authPaths) {
-      app.use(`${baseUrl}${p}`, authMiddleware)
+      app.use(`${baseUrl}${p}`, authMiddleware);
     }
 
     // app middlewares
-    
   }
 }
 
